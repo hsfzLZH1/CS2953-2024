6c6a6fcffcce61074ae695097c1ea93455313a85
hsfzLZH1 <hsfzlzh@outlook.com>
2024-03-15 18:55:35 -0700

    finish lab util and sys call

--- user/pingpong.c
+++ user/pingpong.c
@@ -5,25 +5,27 @@
 int
 main(int argc, char *argv[])
 {
-  int p[2];//the pipe
-  char buf[4];//buffer
-  pipe(p);
+  int p0[2];//the pipe from parent to child
+  int p1[2];//the pipe from child to parent
+  char buf[4]="a";//buffer
+  pipe(p0);
+  pipe(p1);
 
   if(fork()==0)//child
   {
-    read(p[0],buf,1);
-    close(p[0]);
+    close(p0[1]);
+    close(p1[0]);//the child only use 2 fd
+    read(p0[0],buf,1);
     printf("%d: received ping\n",getpid());
-    write(p[1],buf,1);
-    close(p[1]);
+    write(p1[1],buf,1);
   }
   else //parent
   {
-    write(p[1],buf,1);//send a byte to child
-    close(p[1]);
+    close(p0[0]);
+    close(p1[1]);//the parent only use 2 fd
+    write(p0[1],buf,1);//send a byte to child
     wait((int*)0);
-    read(p[0],buf,1);//read the byte from the child
-    close(p[0]);
+    read(p1[0],buf,1);//read the byte from the child
     printf("%d: received pong\n",getpid());
   }
 
