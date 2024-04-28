fafbf2f974f445e219d441c7fb4ab14f28ab3486
hsfzLZH1 <hsfzlzh@outlook.com>
2024-04-27 21:12:21 -0700

    finish Lab5 pgtbl task2

--- kernel/vm.c
+++ kernel/vm.c
@@ -273,6 +273,29 @@ uvmdealloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz)
   return newsz;
 }
 
+// Print the content of pagetable
+void
+vmprint(pagetable_t pagetable)
+{
+  static int depth=0;
+  if(depth==0)printf("page table %p\n",pagetable);
+
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+    if(pte&PTE_V){
+      // this PTE points to a page child.
+      uint64 child = PTE2PA(pte);
+
+      depth++;
+      for(int j=0;j<depth;j++){if(j)printf(" ");printf("..");}
+      printf("%d: pte %p pa %p\n",i,pte,PTE2PA(pte));
+      if((pte&(PTE_R|PTE_W|PTE_X))==0)// child is a lower-level page table
+        vmprint((pagetable_t)child);
+      depth--;
+    }
+  }
+}
+
 // Recursively free page-table pages.
 // All leaf mappings must already have been removed.
 void
